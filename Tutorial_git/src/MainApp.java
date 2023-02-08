d278ad614b2b25b73ed6a3c18d71d1582beab97c
Celia_RD <cerodi91@gmail.com>
2023-02-08 17:11:25 +0100

    dejame mergear por favor

--- Tutorial_git/src/MainApp.java
+++ Tutorial_git/src/MainApp.java
@@ -7,6 +7,7 @@ public class MainApp {
 		//hola soy un nuevo comentario
 		System.out.println("Git nunca funciona a la primera");
 		System.out.println("Git nunca funciona ni a la segunda");
+		System.out.println("No me deja mergear las brnaches");
 	}
 
 }
