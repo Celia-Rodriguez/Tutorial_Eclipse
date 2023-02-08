914d612368359517aaae0f47aff559aca805a9c4
Celia_RD <cerodi91@gmail.com>
2023-02-08 17:06:24 +0100

    tercer commit

--- Tutorial_git/src/MainApp.java
+++ Tutorial_git/src/MainApp.java
@@ -4,6 +4,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		//hola soy un nuevo comentario
+		System.out.println("Git nunca funciona a la primera");
 	}
 
 }
