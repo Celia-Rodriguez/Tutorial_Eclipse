187c336e49f65913f87a8c1266571a21b88a7d35
Celia_RD <cerodi91@gmail.com>
2023-02-15 20:43:38 +0100

    celia

--- Tutorial_git/src/MainApp.java
+++ Tutorial_git/src/MainApp.java
@@ -5,10 +5,8 @@ public class MainApp {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
 		//hola soy un nuevo comentario
-		System.out.println("Git nunca funciona a la primera");
-		System.out.println("Git nunca funciona ni a la segunda");
-		System.out.println("No me deja mergear las brnaches");
-		System.out.println("a ver si ahora funciona");
+		System.out.println("GIT GIT GIT GIT");
+
 	}
 
 }
